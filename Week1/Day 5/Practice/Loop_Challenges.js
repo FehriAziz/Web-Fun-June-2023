6df75030e4a3afa97e9fe1ac4cf457c1e623a413
FehriAziz <azizfehri1@gmail.com>
2023-06-30 13:42:57 +0100

    Loop_Challenges

---
+++ Week1/Day 5/Practice/Loop_Challenges.js
@@ -0,0 +1,32 @@
+var x = 0
+while (x < 20) {
+    x++;
+    if (x % 2  !== 0)
+    console.log(x);
+}
+
+var x = 0;
+while (x < 100) {
+    x++;
+    if (x % 3 === 0)
+    console.log(x);
+}
+
+var  sequence = [4, 2.5, 1, -0.5, -2, -3.5];
+
+for (var i = 0; i < sequence.length; i++) {
+    console.log(sequence[i]);
+}
+
+
+var sum = 0
+for (var i = 0 ; i<101 ; i++)
+    sum = sum + i
+console.log(sum);
+
+var product = 1
+for (var i =1 ; i<13; i++)
+    product = product * i
+console.log(product);
+
+
