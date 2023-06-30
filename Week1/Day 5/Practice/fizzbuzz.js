d51da31e1d19eba924d2d2cf024657c2e543c7f3
FehriAziz <azizfehri1@gmail.com>
2023-06-30 12:44:13 +0100

    fizzbuzz

---
+++ Week1/Day 5/Practice/fizzbuzz.js
@@ -0,0 +1,12 @@
+var i = 0;
+while(i<100) {
+    i++;
+    if (i % 3 === 0) 
+    console.log("fizz");
+    if (i% 5 === 0)
+    console.log("buzz");
+    if (i % 15 === 0)
+    console.log ("fizzbuzz")
+
+    console.log(i);
+}
