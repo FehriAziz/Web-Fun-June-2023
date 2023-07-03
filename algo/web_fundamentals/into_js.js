530288918434cd20389860766450b5cbe07b1180
FehriAziz <azizfehri1@gmail.com>
2023-07-03 16:02:08 +0100

    Week3

---
+++ algo/web_fundamentals/into_js.js
@@ -0,0 +1,27 @@
+// COMMENT
+/*
+CS
+SC
+SC
+*/ 
+
+var i ;
+i = 10;
+var firstName ="john"
+var lastName ="James"
+
+// VARIABLES
+
+//1.STRING 
+var firstName ="john"
+var lastName ="James"
+//2.NUMBER
+var age = 41
+var bmi = 2.5
+//BOOLEAN
+var hasCovid = false 
+var isFunny = true
+ 
+
+
+console.log(`User first name is ${firstName} , last name is ${lastName} `)
